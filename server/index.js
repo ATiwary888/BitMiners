a02c40f8fe4df285fcd72bb02b1624f0bb409005
Vikash Tiwary <vt05665@gmail.com>
2021-05-01 07:27:22 +0530

    Initial Commit

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
-//start...
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+const PORT = 5000;
+app.get('/',function(req,res){
+    res.send('Keep Coding...');
+})
+
+app.listen(PORT,()=>{console.log("server running cool!");});
